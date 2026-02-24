60640cde378f4d1cddfe82cbe37c61bf752130f4
Mikael Arvola <marvel@illinois.edu>
2026-02-23 18:19:30 -0600

    Fix missing plugin import

--- demo/plugins/grad-vue-rte.ts
+++ demo/plugins/grad-vue-rte.ts
@@ -1,4 +1,7 @@
 import { defineNuxtPlugin } from "#imports";
+import GradVueRTE from '@illinois-grad/grad-vue-rte/plugin';
+import '@illinois-grad/grad-vue-rte/grad-vue-rte.css';
 
 export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.use(GradVueRTE);
 })
