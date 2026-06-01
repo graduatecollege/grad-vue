9074a8e40bac21765006feebbb93d05a3c5067ae
Mikael Arvola <marvel@illinois.edu>
2026-06-01 14:04:54 -0500

    Address code review notes

--- packages/grad-vue/src/compose/useScrollRestore.ts
+++ packages/grad-vue/src/compose/useScrollRestore.ts
@@ -140,16 +140,16 @@ export function useScrollRestore(
         }
 
         restoreScrollPosition();
-
-        if (typeof ResizeObserver !== "undefined") {
-            scrollRestoreObserver = new ResizeObserver(() => {
-                restoreScrollPosition();
-            });
-            scrollRestoreObserver.observe(elementRef.value);
-            scrollRestoreObserver.observe(scrollableParent);
+        if (isPendingScrollRestore.value) {
+            if (typeof ResizeObserver !== "undefined") {
+                scrollRestoreObserver = new ResizeObserver(() => {
+                    restoreScrollPosition();
+                });
+                scrollRestoreObserver.observe(elementRef.value);
+                scrollRestoreObserver.observe(scrollableParent);
+            }
+            observeVisibilityChanges(elementRef.value);
         }
-        observeVisibilityChanges(elementRef.value);
-
         scrollableParent.addEventListener("scroll", handleParentScroll);
     });
 
